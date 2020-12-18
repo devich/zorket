8d803e2af78fe184703e7c3b9bfbf18de1d16b75
devich <devich@gmail.com>
2020-12-18 18:49:14 +0200

    some minor changes

---
+++ assets/zorket.js
@@ -0,0 +1,6 @@
+function ready() {
+    //$('.spr-badge[data-rating="0.0"] > span.spr-badge-caption').hide();
+	console.log( "ready!" );
+}
+  
+document.addEventListener("DOMContentLoaded", ready);
